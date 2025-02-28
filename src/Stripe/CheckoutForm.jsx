e1ec0fe7347ec8f30a8011ca8c6ff415c1527873
marinahradway <headway2121@mail.com>
2025-02-28 17:16:21 +0300

    changed port

--- src/Stripe/CheckoutForm.jsx
+++ src/Stripe/CheckoutForm.jsx
@@ -18,7 +18,7 @@ export const CheckoutForm = () => {
       try {
         const { id } = paymentMethod;
         const response = await axios.post(
-          "http://localhost:8080/stripe/charge",
+          "http://localhost:8070/stripe/charge",
           {
             amount: 999,
             id: id,
